22d7a499c9a6e3409cafea9782a352ea9327c3cd
PrayasPathak <prayasroman1@gmail.com>
2023-11-06 09:38:23 +0545

    Rendering dynamic pages.

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, jsonify
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db
 
 app = Flask(__name__)
 
@@ -18,6 +18,20 @@ def index():
 @app.route("/api/jobs")
 def list_jobs():
     return jsonify(jobs)
+
+@app.route("/api/job/<id>")
+def list_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return jsonify(job)
+
+@app.route("/job/<id>")
+def show_job(id):
+    job = load_job_from_db(id)
+    if not job:
+        return "Not Found", 404
+    return render_template("jobpage.html", job=job)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- database.py
+++ database.py
@@ -2,7 +2,6 @@ from sqlalchemy import create_engine, text
 import os
 
 db_connection_string = os.environ['DB_CONNECTION_STRING']
-print(db_connection_string)
 
 engine = create_engine(db_connection_string)
 
@@ -14,3 +13,16 @@ def load_jobs_from_db():
         for row in result.all():
             jobs.append(row._asdict())
         return jobs
+    
+def load_job_from_db(id):
+    with engine.connect() as conn:
+        result = conn.execute(
+            text("SELECT * FROM jobs WHERE id = :val"),
+            {"val":id})
+        rows = result.all()
+
+        if len(rows) == 0:
+            return None
+        else:
+            return rows[0]._asdict()
+
