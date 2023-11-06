611daf6d7c668cf9c06d061c023aebd117e38add
PrayasPathak <prayasroman1@gmail.com>
2023-11-06 16:15:03 +0545

    Inserting data in the database and making form fields required.

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
 from flask import Flask, render_template, jsonify, request
-from database import load_jobs_from_db, load_job_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
 
 
-    
-
 jobs = load_jobs_from_db()
-
 
 @app.route("/")
 def index():
@@ -34,11 +31,18 @@ def show_job(id):
     return render_template("jobpage.html", job=job)
 
 
-@app.route("/job/<id>/apply", methods=['post'])
+@app.route("/job/<id>/apply", methods=['POST'])
 def apply_to_job(id):
     data = request.form
     job = load_job_from_db(id)
-    return render_template("application_submitted.html", application=data, job=job)
+    
+    add_application_to_db(id, data)
+    
+    return render_template(
+        "application_submitted.html", 
+        application=data, 
+        job=job
+        )
 
 if __name__ == "__main__":
     app.run(debug=True)
